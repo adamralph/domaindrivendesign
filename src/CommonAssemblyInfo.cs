86367a8be2bb05251a875e9a72e50d5db452550f
Adam Ralph <adam@adamralph.com>
2012-12-02 13:25:33 +0100

    added solution and solution files

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="CommonAssemblyInfo.cs" company="DomainDrivenDesign contributors">
+//  Copyright (c) DomainDrivenDesign contributors. All rights reserved.
+// </copyright>
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DomainDrivenDesign")]
+[assembly: AssemblyCopyright("Copyright © DomainDrivenDesign. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("DomainDrivenDesign")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha01")]
+[assembly: SuppressMessage(
+    "Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Using SemVer in AssemblyInformationalVersion.")]
+
+[assembly: CLSCompliant(true)]
