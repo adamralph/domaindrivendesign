dee272b147273f0cb7276f30d6ae2e58acfd9685
Adam Ralph <adam@adamralph.com>
2012-12-02 13:36:26 +0100

    added main library project

---
+++ src/DomainDrivenDesign/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="AssemblyInfo.cs" company="DomainDrivenDesign contributors">
+//  Copyright (c) DomainDrivenDesign contributors. All rights reserved.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DomainDrivenDesign")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ComVisible(true)]
+[assembly: Guid("3cb751d5-daa6-4ebd-9e6f-958affdf7efb")]
