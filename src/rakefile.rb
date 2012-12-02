92dce664aab31b317c5332b15e0647add90887de
Adam Ralph <adam@adamralph.com>
2012-12-02 15:31:56 +0100

    add output task to rakefile.rb

--- src/rakefile.rb
+++ src/rakefile.rb
@@ -1,6 +1,18 @@
 require 'albacore'
+require 'fileutils'
 
-task :default => [ :build ]
+task :default => [ :output ]
+
+desc "Output"
+output :output => [:build] do |out|
+  FileUtils.rmtree "bin"
+  FileUtils.mkpath "bin/lib/net35"
+  
+  out.from "DomainDrivenDesign/bin/Release"
+  out.to "bin/lib/net35"
+  out.file "DomainDrivenDesign.dll"
+  out.file "DomainDrivenDesign.xml"
+end
 
 desc "Build"
 msbuild :build => :clean do |msb|
