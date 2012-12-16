82a0d8276e204cfdaa09fa586ec5a8a76a76fd0f
Adam Ralph <adam@adamralph.com>
2012-12-16 22:07:21 +0100

    added AggregateRoot

---
+++ src/DomainDrivenDesign/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="AggregateRoot.cs" company="DomainDrivenDesign contributors">
+//  Copyright (c) DomainDrivenDesign contributors. All rights reserved.
+// </copyright>
+
+namespace DomainDrivenDesign
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// An entity which is the root of an aggregate.
+    /// </summary>
+    /// <typeparam name="TIdentity">The type of the identity.</typeparam>
+    [Serializable]
+    [ComVisible(true)]
+    public abstract class AggregateRoot<TIdentity> : Entity<TIdentity>
+    {
+    }
+}
