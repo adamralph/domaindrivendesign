4c40b738ce5da150cab3fb285821050525b23f06
Adam Ralph <adam@adamralph.com>
2012-12-26 22:52:44 +0100

    refactored rakefile - removed task dependencies and reordered tasks in execution order

--- src/rakefile.rb
+++ src/rakefile.rb
@@ -2,31 +2,8 @@ require 'rubygems'
 require 'albacore'
 require 'fileutils'
 
-task :default => [ :nugetpack ]
-task :mono => [ :buildmono ]
-
-desc "Create the nuget package"
-nugetpack :nugetpack => [ :spec ] do |nuget|
-  FileUtils.mkpath "bin"
-  
-  nuget.command     = "../packages/NuGet.CommandLine.2.1.2/tools/NuGet.exe"
-  nuget.nuspec      = "DomainDrivenDesign.nuspec"
-  nuget.output      = "bin"
-end
-
-desc "Execute specs"
-xunit :spec => :build do |xunit|
-  xunit.command = "../packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
-  xunit.assembly = "test/DomainDrivenDesign.Specs/bin/Debug/DomainDrivenDesign.Specs.dll"
-  xunit.html_output = "test/DomainDrivenDesign.Specs/bin/Debug"
-end
-
-desc "Build solution"
-msbuild :build => :clean do |msb|
-  msb.properties = { :configuration => :Release }
-  msb.targets = [ :Build ]
-  msb.solution = "DomainDrivenDesign.sln"
-end
+task :default => [ :clean, :build, :spec, :nugetpack ]
+task :mono => [ :cleanmono, :buildmono ]
 
 desc "Clean solution"
 msbuild :clean do |msb|
@@ -35,18 +12,39 @@ msbuild :clean do |msb|
   msb.solution = "DomainDrivenDesign.sln"
 end
 
-desc "Build solution"
+desc "Clean solution"
+xbuild :cleanmono do |xb|
+  xb.properties = { :configuration => :Release }
+  xb.targets = [ :Clean ]
+  xb.solution = "DomainDrivenDesign.sln"
+end
 
-xbuild :buildmono => :cleanmono do |xb|
+desc "Build solution"
+msbuild :build do |msb|
+  msb.properties = { :configuration => :Release }
+  msb.targets = [ :Build ]
+  msb.solution = "DomainDrivenDesign.sln"
+end
+
+desc "Build solution"
+xbuild :buildmono do |xb|
   xb.properties = { :configuration => :Release }
   xb.targets = [ :Build ]
   xb.solution = "DomainDrivenDesign.sln"
 end
 
-desc "Clean solution"
+desc "Execute specs"
+xunit :spec do |xunit|
+  xunit.command = "../packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
+  xunit.assembly = "test/DomainDrivenDesign.Specs/bin/Debug/DomainDrivenDesign.Specs.dll"
+  xunit.html_output = "test/DomainDrivenDesign.Specs/bin/Debug"
+end
 
-xbuild :cleanmono do |xb|
-  xb.properties = { :configuration => :Release }
-  xb.targets = [ :Clean ]
-  xb.solution = "DomainDrivenDesign.sln"
+desc "Create the nuget package"
+nugetpack :nugetpack do |nuget|
+  FileUtils.mkpath "bin"
+  
+  nuget.command = "../packages/NuGet.CommandLine.2.1.2/tools/NuGet.exe"
+  nuget.nuspec = "DomainDrivenDesign.nuspec"
+  nuget.output = "bin"
 end
