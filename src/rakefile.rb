6a8dcae520728c13c7619a3b0bb9cd95588e945c
Adam Ralph <adam@adamralph.com>
2012-12-29 11:59:00 +0000

    changed rakefile to internally choose .NET or Mono based on OS with manual override

--- src/rakefile.rb
+++ src/rakefile.rb
@@ -4,76 +4,83 @@ require 'fileutils'
 
 task :default => [ :clean, :build, :spec, :nugetpack ]
 
-desc "Run all Mono tasks"
-task :mono => [ :cleanmono, :buildmono, :specmono, :nugetpackmono ]
-
-desc "Clean solution"
-msbuild :clean do |msb|
-  msb.properties = { :configuration => :Release }
-  msb.targets = [ :Clean ]
-  msb.solution = "DomainDrivenDesign.sln"
+desc "Forces use of mono for all subsequent tasks"
+task :mono do
+  ENV["FORCEMONO"] = "FORCEMONO"
 end
 
-desc "Clean solution using Mono"
-xbuild :cleanmono do |xb|
-  xb.properties = { :configuration => :Release }
-  xb.targets = [ :Clean ]
-  xb.solution = "DomainDrivenDesign.sln"
+desc "Clean solution"
+task :clean do
+  FileUtils.rmtree "bin"
+
+  if use_mono then
+    build = XBuild.new
+  else
+    build = MSBuild.new
+  end
+  
+  build.properties = { :configuration => :Release }
+  build.targets = [ :Clean ]
+  build.solution = "DomainDrivenDesign.sln"
+  build.execute
 end
 
 desc "Build solution"
-msbuild :build do |msb|
-  msb.properties = { :configuration => :Release }
-  msb.targets = [ :Build ]
-  msb.solution = "DomainDrivenDesign.sln"
-end
-
-desc "Build solution using Mono"
-xbuild :buildmono do |xb|
-  ENV["BuildSystem"] = "xbuild"
-  xb.properties = { :configuration => :Release }
-  xb.targets = [ :Build ]
-  xb.solution = "DomainDrivenDesign.sln"
+task :build do
+  if use_mono then
+    ENV["MONO"] = "MONO"
+    build = XBuild.new
+  else
+    ENV["MONO"] = ""
+    build = MSBuild.new
+  end
+  
+  build.properties = { :configuration => :Release }
+  build.targets = [ :Build ]
+  build.solution = "DomainDrivenDesign.sln"
+  build.execute
 end
 
 desc "Execute specs"
 xunit :spec do |xunit|
-  xunit.command = "../packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
-  xunit.assembly = "test/DomainDrivenDesign.Specs/bin/Debug/DomainDrivenDesign.Specs.dll"
-  xunit.html_output = "test/DomainDrivenDesign.Specs/bin/Debug"
-end
-
-desc "Execute specs using Mono"
-xunit :specmono do |xunit|
-  if ENV["OS"] == "Windows_NT"
-    xunit.command = "xunitmono.bat"
+  if use_mono then
+    if ENV["OS"] == "Windows_NT"
+      xunit.command = "xunitmono.bat"
+    else
+      xunit.command = "xunitmono.sh"
+    end
   else
-    xunit.command = "xunitmono.sh"
-  end
+    xunit.command = "../packages/xunit.runners.1.9.1/tools/xunit.console.clr4.exe"
+  end if
+  
   xunit.assembly = "test/DomainDrivenDesign.Specs/bin/Debug/DomainDrivenDesign.Specs.dll"
   xunit.options "/html test/DomainDrivenDesign.Specs/bin/Debug/DomainDrivenDesign.Specs.dll.html"
 end
 
-# NOTE (Adam): nuspec files can be consolidated after NuGet 2.3 is released - see http://nuget.codeplex.com/workitem/2767
 desc "Create the nuget package"
 nugetpack :nugetpack do |nuget|
   FileUtils.mkpath "bin"
   
-  nuget.command = "../packages/NuGet.CommandLine.2.2.0/tools/NuGet.exe"
-  nuget.nuspec = "DomainDrivenDesign.Windows_NT.nuspec"
+  if use_mono then
+    if ENV["OS"] == "Windows_NT"
+      nuget.command = "nugetmono.bat"
+    else
+      nuget.command = "nugetmono.sh"
+    end
+  else
+    nuget.command = "../packages/NuGet.CommandLine.2.2.0/tools/NuGet.exe"
+  end
+
+  # NOTE (Adam): nuspec files can be consolidated after NuGet 2.3 is released - see http://nuget.codeplex.com/workitem/2767
+  if ENV["OS"] == "Windows_NT"
+    nuget.nuspec = "DomainDrivenDesign.Windows_NT.nuspec"
+  else
+    nuget.nuspec = "DomainDrivenDesign.nuspec"
+  end
+  
   nuget.output = "bin"
 end
 
-desc "Create the nuget package using Mono"
-nugetpack :nugetpackmono do |nuget|
-  FileUtils.mkpath "bin"
-  
-  if ENV["OS"] == "Windows_NT"
-    nuget.command = "nugetmono.bat"
-    nuget.nuspec = "DomainDrivenDesign.Windows_NT.nuspec"
-  else
-    nuget.command = "nugetmono.sh"
-    nuget.nuspec = "DomainDrivenDesign.nuspec"
-  end
-  nuget.output = "bin"
+def use_mono()
+  return ENV["OS"] != "Windows_NT" || ENV["FORCEMONO"]
 end
